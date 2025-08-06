81eec7fdeb353ba57b6d9453d0fd29b3173b07ac
NuriddinovBunyod <nuriddinov.bunyodbek@icloud.com>
2025-08-06 19:54:39 +0500

    jamoviy

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+
+// function showSideber(){
+// const sideber = document.querySelector('sideber')
+// sideber.style.display='block'
+// }
+// function closeSideber() {
+// const sideber = document.querySelector('sideber')
+// sideber.style.display = 'none'
+// }
+function showSideber() {
+    document.getElementById("sideber").style.display = "flex";
+}
+
+function closeSideber() {
+    document.getElementById("sideber").style.display = "none";
+}
